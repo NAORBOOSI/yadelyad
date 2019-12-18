16aeeecfe63bfe35fe65717682e0b04b0927961d
NAORBOOSI <naorboosi@gmail.com>
2019-12-18 13:41:42 +0200

    create delivery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateDeliveryComponent } from './component/create-delivery/create-delivery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateDeliveryComponent
   ],
   imports: [
     BrowserModule
